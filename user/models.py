66774ea8fcb665c6b06ffcdd84d842f5ea90fdaa
sanvit <sanvit@sanvit.me>
2021-05-21 15:00:21 +0000

    added models

---
+++ user/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+import uuid
+from django.utils import timezone
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+# Create your models here.
+
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def create_user(self, username, password, nickname):
+        user = self.model(nickname=nickname, username=username)
+        if password:
+            user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, nickname, username, password):
+        user = self.model(nickname=nickname, username=username, date_joined=timezone.now)
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    """ User Model """
+
+    objects = UserManager()
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False, blank=False, auto_created=True)
+    username = models.CharField(max_length=64, unique=True)
+    nickname = models.CharField(max_length=64, null=False, blank=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    @property
+    def __str__(self):
+        return self.username
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["username", "nickname"]
--- zzim/models.py
+++ zzim/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+class item(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, blank=False, null=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    price = models.PositiveIntegerField(null=False, blank=False, default=0)
+    shipping = models.PositiveIntegerField(null=False, blank=False, default=0)
+    mall = models.ForeignKey("shoppingMall", on_delete=models.SET_NULL, null=True)
+    url = models.URLField(null=False, blank=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    is_purchased = models.BooleanField(default=False, null=False, blank=False)
+    is_shared = models.BooleanField(default=False, null=False, blank=False)
+
+
+class shoppingMall(models.Model):
+    url = models.URLField(null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    icon = models.URLField(null=False, blank=False)
+    logo = models.URLField(null=False, blank=False)

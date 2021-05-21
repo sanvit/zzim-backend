095cd58002f7f77e2cfde8fd7baddca774c59b73
rxb8k <82093280+rxb8k@users.noreply.github.com>
2021-05-22 04:58:12 +0900

    Add files via upload

---
+++ zzim/parsers/naver.py
@@ -0,0 +1,29 @@
+from zzim.models import item
+import requests
+from bs4 import BeautifulSoup, MarkupResemblesLocatorWarning
+
+from parsers.naver import parser as naverparser
+
+def parser(url):
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    new_item = item()
+
+    name = soup.find('h3', {'class': '_3oDjSvLwq9 _copyable'}).text
+    new_item.name = name
+
+    price=soup.find('span',{'class':'_1LY7DqCnwR'}).text
+    new_item.price = price.replace(",","")
+
+    is_moobae=soup.find('span',{'class':'Y-_Vd4O6dS'}).text
+    if(is_moobae=="무료배송") : shipping=0
+    else :
+        shipping=soup.find('span',{'class':'Y-_Vd4O6dS'}).find('span','_1_wrVRMvuL').text
+    new_item.shipping=shipping.replace(",","")
+
+    new_item.mall = '네이버 스마트스토어'
+
+    new_item.image_url=soup.find('div',{'class':'_23RpOU6xpc'}).find('img')['src']
+
+    new_item.save()
+    return new_item

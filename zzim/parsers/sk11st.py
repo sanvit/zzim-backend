631574a31087a6b39a8ae6f0b5028568e076d5d2
hgo641 <hgo641@gmail.com>
2021-05-26 01:14:46 +0900

    update sk11st.py

--- zzim/parsers/sk11st.py
+++ zzim/parsers/sk11st.py
@@ -3,15 +3,18 @@ import requests
 from bs4 import BeautifulSoup
 from zzim.models import item, shoppingMall
 
-
 def parser(url):
-    #    prdNo = parse_qs(urlparse(url).query).get('prdNo')[0]\
-    #    pcurl = f"http://www.11st.co.kr/products/{prdNo}"
-    #    print(url)
+    #   prdNo = ""
+    # if parse_qs(urlparse(url).query).get('prdNo'):
+    #     prdNo = parse_qs(urlparse(url).query).get('prdNo')
+    # elif parse_qs(urlparse(url).query).get('prdNo'):
+    #     prdNo = parse_qs(urlparse(url).query).get('prdNo')
+    # else:
+    #     prdNo = parse_qs(urlparse(url)).get('prdNo')
     request = requests.get(url)
     soup = BeautifulSoup(request.text, 'html.parser')
     name = soup.find('h1', {'class': 'title'}).text
-    price_str = soup.find('span', {'class': 'value'}).text
+    price_str = soup.find('dl',{'class':'price'}).find('span', {'class': 'value'}).text
     price = ""
     for c in price_str:
         if c.isdigit():
@@ -31,14 +34,7 @@ def parser(url):
         shipping = int(shipping)
 
     image_url = soup.find('meta', {'property': 'og:image'}).get('content')
-    print(price)
+
     # 모델에 저장
-    new_item = item()
-    new_item.name = name
-    new_item.price = price
-    new_item.shipping = shipping
-    new_item.mall = shoppingMall.objects.get(slug='11st')
-    new_item.url = url
-    new_item.image_url = image_url
-    new_item.save()
-    return new_item
+  
+    return 

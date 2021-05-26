59d3ca75abdfb27ced9dba8b5034ac16e065f86d
sanvit <sanvit@sanvit.me>
2021-05-26 16:21:53 +0000

    add ensure ascii

--- user/views.py
+++ user/views.py
@@ -17,7 +17,8 @@ def join(request):
         user = User.objects.create_user(username, password, nickname)
         user.save()
         auth.login(request, user)
-        return JsonResponse({"status": "SUCCESS", "message": "성공적으로 가입되었습니다."})
+        return JsonResponse({"status": "SUCCESS", "message": "성공적으로 가입되었습니다."},
+                            json_dumps_params={'ensure_ascii': False})
 
 
 @csrf_exempt
@@ -29,10 +30,18 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                return JsonResponse({"status": "SUCCESS", "message": "성공적으로 로그인되었습니다."})
-        return JsonResponse({"status": "FAILED", "message": "사용자 정보가 일치하지 않습니다."}, status=403)
+                return JsonResponse({"status": "SUCCESS", "message": "성공적으로 로그인되었습니다."},
+                                    json_dumps_params={'ensure_ascii': False})
+        return JsonResponse(
+            {"status": "FAILED", "message": "사용자 정보가 일치하지 않습니다."},
+            json_dumps_params={'ensure_ascii': False},
+            status=403)
+    return JsonResponse(
+        {"status": "ALREADY_LOGGED_IN", "message": "이미 로그인되어 있습니다."},
+        json_dumps_params={'ensure_ascii': False},
+        status=400)
 
 
 def logout(request):
     auth.logout(request)
-    return JsonResponse({"status": "SUCCESS", "message": "성공적으로 로그아웃되었습니다."})
+    return JsonResponse({"status": "SUCCESS", "message": "성공적으로 로그아웃되었습니다."}, json_dumps_params={'ensure_ascii': False})
--- zzim/views.py
+++ zzim/views.py
@@ -17,7 +17,7 @@ def listItem(req):
                      'shippingPrice': i.shipping, 'shoppingMallName': i.mall.name, 'logoImage': i.mall.logo,
                      'createdDate': i.date_added, 'url': i.url, 'purchased': i.is_purchased}
         item_list.append(item_json)
-    return JsonResponse({"status": "SUCCESS", "data": item_list})
+    return JsonResponse({"status": "SUCCESS", "data": item_list}, json_dumps_params={'ensure_ascii': False})
 
 
 def viewItem(req, id):
@@ -28,7 +28,10 @@ def viewItem(req, id):
              'shippingPrice': item_object.shipping, 'shoppingMallName': item_object.mall.name,
              'logoImage': item_object.mall.logo, 'createdDate': item_object.date_added, 'url': item_object.url,
              'purchased': item_object.is_purchased})
-    return JsonResponse({"status": "FAILED", "message": "사용자 정보가 일치하지 않습니다."}, status=403)
+    return JsonResponse(
+        {"status": "FAILED", "message": "사용자 정보가 일치하지 않습니다."},
+        json_dumps_params={'ensure_ascii': False},
+        status=403)
 
 
 @csrf_exempt
@@ -40,11 +43,15 @@ def editItem(req, id):
             item_object.price = req.POST['price']
             item_object.shipping = req.post['shipping']
             item_object.save()
-            return JsonResponse({"status": "SUCCESS", "message": "성공적으로 수정되었습니다."})
+            return JsonResponse({"status": "SUCCESS", "message": "성공적으로 수정되었습니다."},
+                                json_dumps_params={'ensure_ascii': False})
         return JsonResponse({'image': item_object.image_url, 'name': item_object.name, 'price': item_object.price,
                              'shippingPrice': item_object.shipping, 'logoImage': item_object.mall.logo,
                              'createdDate': item_object.date_added, 'url': item_object.url})
-    return JsonResponse({"status": "FAILED", "message": "사용자 정보가 일치하지 않습니다."}, status=403)
+    return JsonResponse(
+        {"status": "FAILED", "message": "사용자 정보가 일치하지 않습니다."},
+        json_dumps_params={'ensure_ascii': False},
+        status=403)
 
 
 def setPurchasedItem(req, id):
@@ -78,12 +85,18 @@ def addItem(req):
                     item_object = itemUrlParser(url)
                     item_object.user = req.user
                     item_object.save()
-                    return JsonResponse({'status': "SUCCESS", 'message': "아이템이 정상적으로 저장되었습니다.", 'id': item_object.uuid})
+                    return JsonResponse(
+                        {'status': "SUCCESS", 'message': "아이템이 정상적으로 저장되었습니다.", 'id': item_object.uuid},
+                        json_dumps_params={'ensure_ascii': False})
                 except:
                     return JsonResponse(
                         {'status': "FAILED", 'message': "현재 지원하지 않는 쇼핑몰이거나 저장중 오류가 발생하였습니다."},
+                        json_dumps_params={'ensure_ascii': False},
                         status=400)
-    return JsonResponse({'status': 'FAILED', 'message': '잘못된 요청입니다.'}, status=400)
+    return JsonResponse(
+        {'status': 'FAILED', 'message': '잘못된 요청입니다.'},
+        json_dumps_params={'ensure_ascii': False},
+        status=400)
 
 
 def viewOtherUserItem(req, id):
@@ -96,5 +109,10 @@ def viewOtherUserItem(req, id):
                          'shippingPrice': i.shipping, 'shoppingMallName': i.mall.name, 'logoImage': i.mall.logo,
                          'createdDate': i.date_added, 'url': i.url}
             item_list.append(item_json)
-        return JsonResponse({"status": "SUCCESS", "nickname": user.nickname, "data": item_list})
-    return JsonResponse({"status": "FAILED", "message": "비공개 프로필입니다."}, status=403)
+        return JsonResponse(
+            {"status": "SUCCESS", "nickname": user.nickname, "data": item_list},
+            json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(
+        {"status": "FAILED", "message": "비공개 프로필입니다."},
+        json_dumps_params={'ensure_ascii': False},
+        status=403)

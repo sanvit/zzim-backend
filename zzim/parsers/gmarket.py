d917e2b84e570746b283ee0c7c52312bfe6deeef
hgo641 <hgo641@gmail.com>
2021-05-29 22:11:42 +0900

    modify gmarket.py

--- zzim/parsers/gmarket.py
+++ zzim/parsers/gmarket.py
@@ -5,7 +5,41 @@ from bs4 import BeautifulSoup
 
 
 def parser(url):
+    if "mitem" in url:
+        request = requests.get(url)
+        soup = BeautifulSoup(request.text, 'html.parser')
+        name = soup.find('div',{'class':'box__item-title'}).find('h3').text
+        price_str = soup.find('span',{'class':'text__price'}).find('span',{'class':'text_number'}).text
+        price = ""
+        for c in price_str:
+            if c.isdigit():
+                price = price + c
+        if price == "":
+            price = 0
+        else:
+            price = int(price)
+
+        shipping_str = soup.find('span',{'class':'text__branch'}).text
+        shipping = ""
+        for c in shipping_str:
+            if c.isdigit():
+                shipping = shipping + c
+        if shipping == "":
+            shipping = 0
+        else:
+            shipping = int(shipping)
+        image_url = soup.find('meta', {'property': 'og:image'}).get('content')
+        new_item = item()
+        new_item.name = name
+        new_item.price = price
+        new_item.shipping = shipping
+        new_item.mall = shoppingMall.objects.get(slug='gmarket')
+        new_item.url = url
+        new_item.image_url = image_url
+        new_item.save()
+        return new_item
     request = requests.get(url)
+    print(url)
     soup = BeautifulSoup(request.text, 'html.parser')
     name = soup.find('h1', {'class': 'itemtit'}).text
     price_str = soup.find('strong', {'class': 'price_real'}).text
@@ -28,6 +62,7 @@ def parser(url):
     else:
         shipping = int(shipping)
     image_url = soup.find('meta', {'property': 'og:image'}).get('content')
+
     # 모델에 저장
     new_item = item()
     new_item.name = name
